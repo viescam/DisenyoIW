1f1c1a67ef7c9f2185770db5b127e587ae05640f
alumno <alumno@2A5PC06.fpmislata.local>
2016-12-13 13:55:35 +0100

    Cambios en dashboardBootstrap

---
+++ dashboardBootstrap/web/js/navbar.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+(function($) {
+    var $window = $(window),
+        $html = $('#menu-bar');
+ 
+    $window.resize(function resize() {
+        if ($window.width() < 768) {
+           return $html.removeClass('nav-stacked');
+        }
+        $html.addClass('nav-stacked');
+    }).trigger('resize');
+})(jQuery);
+</script>
+

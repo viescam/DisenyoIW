33a02028f654defa79a9beaaa8edcc27afc85862
alumno <alumno@2A5PC06.fpmislata.local>
2016-12-15 11:39:14 +0100

    Puzzleando

---
+++ puzzle/public_html/assests/js/puzzle.js
@@ -0,0 +1,32 @@
+function Puzzle(){}
+
+Puzzle.prototype.onDragStartPiezaSinPoner=function(event){
+    
+}
+
+Puzzle.prototype.onDragStartPiezaPuesta=function(event){
+    
+}
+
+Puzzle.prototype.onDropPiezaSinPoner=function(event){
+    event.preventDefault();
+}
+
+Puzzle.prototype.onDropPiezaPuesta=function(event){
+    event.preventDefault();
+}
+
+Puzzle.prototype.onDragOverPiezaSinPoner=function(event){
+    event.preventDefault();
+    
+}
+
+Puzzle.prototype.onDragOverPiezaPuesta=function(event){
+    event.preventDefault();
+}
+
+
+
+
+puzzle = new Puzzle();
+

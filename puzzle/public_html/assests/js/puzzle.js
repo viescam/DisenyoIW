e2a5b4119139a4e5a5f0de21ad9e6062972d33c1
alumno <alumno@2A5PC06.fpmislata.local>
2016-12-20 13:39:44 +0100

    Puzzle funcionando

--- puzzle/public_html/assests/js/puzzle.js
+++ puzzle/public_html/assests/js/puzzle.js
@@ -1,29 +1,39 @@
 function Puzzle(){}
 
 Puzzle.prototype.onDragStartPiezaSinPoner=function(event){
-    
-}
+    event.dataTransfer.setData("text",event.target.id);
+};
 
 Puzzle.prototype.onDragStartPiezaPuesta=function(event){
-    
-}
+    event.dataTransfer.setData("text",event.target.id);
+};
 
 Puzzle.prototype.onDropPiezaSinPoner=function(event){
+    //alert(event.target);
     event.preventDefault();
-}
+    var idNuevo = event.dataTransfer.getData("text");
+    var srcAntigua = document.getElementById(idNuevo).src;
+    document.getElementById(idNuevo).src = event.target.src;
+    event.target.src = srcAntigua;
+};
 
 Puzzle.prototype.onDropPiezaPuesta=function(event){
+    //alert(event.target);
     event.preventDefault();
-}
+    var idNuevo = event.dataTransfer.getData("text");
+    //event.target.replaceChild(document.getElementById(idNuevo),event.target);
+    var srcAntigua = document.getElementById(idNuevo).src;
+    document.getElementById(idNuevo).src = event.target.src;
+    event.target.src = srcAntigua;
+};
 
 Puzzle.prototype.onDragOverPiezaSinPoner=function(event){
-    event.preventDefault();
-    
-}
+    event.preventDefault();    
+};
 
 Puzzle.prototype.onDragOverPiezaPuesta=function(event){
     event.preventDefault();
-}
+};
 
 
 
